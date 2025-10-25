18af6512b68c9023a33204fd16b999f7f9a4ab46
Chris Gitmed <chrisgitmed@Chriss-MacBook-Pro-2.local>
2025-10-24 21:14:01 -0700

    Made progress on checkout script. Now being designed with amazon.com first in mind, and working up to checkout page

--- TODO.md
+++ TODO.md
@@ -1,17 +1,17 @@
 # TODO (MVP)
-1. Create a simple web server using Node.js and Express.js, or a Puppeteer script.
-2. Write a function which goes to specified URLs and accesses desired SKUs.
-3. Use cron jobs to run the function every thirty minutes.
-5. Update the function to actually purchase any found items.
-6. Set up SMS/RCS alerts that an item has been found and/or purchased (optional: do we want to manually confirm purchases once the items are in our cart? If the items are not going to go out of stock once they're in our cart, then yes, else probably not).
-7. Build comprehensive local tests.
-8. Deploy AWS infrastructure.
+1. Create a simple a Puppeteer script. ✅
+2. Update the script to navigate to a targeted URL. ✅
+3. Update the script to click 'Add to Cart' (or equivalent) and navigate to the checkout page. ✅
+4. Update the script to fill in card information and check out.
+5. Set up a cron job to run the script every thirty minutes.
+6. Build comprehensive local tests.
 
 # TODO (After MVP)
 1. Create and connect a simple PostgreSQL database.
 2. Update the function to record logs of each cron run in the database.
+3. Update the script to handle multiple checkout processes at once
+3. Implement SMS/RCS/Email/Discord alerts
 
 # Questions
 1. What should the spending limit be when making purchases?
 2. Which source of funds should be used when making purchases (Paypal, Credit Cards, etc.)?
-3. Should we have the function run more than once every thirty minutes?
--- app/checkout.js
+++ app/checkout.js
@@ -3,6 +3,31 @@ import { exit } from 'node:process';
 import { log } from 'node:console';
 import puppeteer from 'puppeteer';
 
+// Simple logging/debugging function to log 'Clicked' on button click
+const clickButton = async (page, selector) => {
+  await page.$eval(selector, (button) => {
+    button.click();
+  });
+  console.log('Clicked');
+};
+
+// Tell the node process to sleep for `x` milliseconds
+const sleep  = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Define the target URLs
+const targetUrls = [
+  // 'https://www.walmart.com/ip/Scarlet-Violet-Mega-Evolution-Booster-Bundle/17344505131?classType=REGULAR&athbdg=L1102&from=/search',
+  'https://www.amazon.com/Diamond-Crystal-Kosher-Salt-Additives/dp/B0BQPXDF8X/ref=sr_1_1_pp?crid=NR59PY891JAL&dib=eyJ2IjoiMSJ9.mpKpftlM6UTPhBQ1dCHSrDr7eBLPT7388OAdSVHVG-rGY-PU8VNnozQ0X71F2ASWHEUU1-WAP3oW7c-ghz7jD4UE3RyWpxtaIvOHYI0toIUUCOcm2Bo6Xva6VrgDBqtoRW5RcHiZY21wggU54qeYokKVSpD_VKU9PnHP3egXKF27YHrIJnshEKriRxICVQkyA7rBf5_8YXG0R91pohDIaDJEYRDMMA1i4-Hm5gJezColuZDU9Kj1ecD0oiR_D1-UvV8C5CBR4OMdkW6ixAvowMuy4rJQdrtcXHDB3PuRAw0.RYrDam_-4-e1R0ZOOPVtro4kNKCaH8-2yShzGN4DLH8&dib_tag=se&keywords=kosher%2Bsalt&qid=1761362047&sprefix=kosher%2Bsal%2Caps%2C172&sr=8-1&th=1',
+];
+
+// Instantiate the browser, leveraging a proxy server
+const browser = await puppeteer.launch({
+  headless: false,
+  args: [
+    `--proxy-server=http://${config.PROXY_SERVER}:${config.PROXY_SERVER_PORT}`,
+  ],
+});
+
 /**
  * This script should use the puppeteer package to open a new browser window.
  * The browser should then navigate to any specified product URLs, and add the item(s) to the cart.
@@ -10,20 +35,9 @@ import puppeteer from 'puppeteer';
  * any necessary forms along the way. Finally, a notification should be send to the User that the
  * item(s) were checkout out.
  *
- * TODO: Once the above error is fixed, the script should be updated to go directly to the desired product URL
- * TODO: (Not MVP): Send SMS/RCS notification
- * TODO: (Not MVP): Can I update this to visit multiple URLs in parallel?
- *
+ * ! This script needs to be tailored for each specific vendor. MVP will be specific to amazon.com
  */
 (async () => {
-  // Instantiate the browser, leveraging a proxy server to avoid bot detection systems
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      `--proxy-server=http://${config.PROXY_SERVER}:${config.PROXY_SERVER_PORT}`,
-    ],
-  });
-
   try {
     // Open a new page and set authentication credentials
     const page = await browser.newPage();
@@ -32,23 +46,50 @@ import puppeteer from 'puppeteer';
       password: config.PROXY_PASSWORD,
     });
 
-    // Go to the target URL
-    const targetUrl = 'https://www.walmart.com/search?q=Pokemon+Trading+Cards';
-    await page.goto(targetUrl, {
-      waitUntil: 'load',
-      timeout: '180000', // Three minutes
+    // Go to the product URL
+    await page.goto(targetUrls[0], {
+      timeout: '180000',
+      waitUntil: 'domcontentloaded',
     });
+    log('Page loaded');
+    sleep(2000);
 
-    // TODO: Click 'Add To Cart'
+    // Click 'Add To Cart'
+    await Promise.all([
+      page.waitForNavigation({
+        timeout: '180000',
+        waitUntil: 'domcontentloaded',
+      }),
+      clickButton(page, '#add-to-cart-button'),
+    ]);
+    log('Added item to cart');
+    sleep(3000);
 
-    // TODO: Checkout
+    // Click 'Proceed to checkout'
+    await Promise.all([
+      page.waitForNavigation({
+        timeout: '180000',
+        waitUntil: 'domcontentloaded',
+      }),
+      clickButton(page, 'input[name="proceedToRetailCheckout"]'),
+    ]);
+    log('Proceeded to checkout');
+    sleep(3000);
+
+    // TODO: Enter any need checkout details
+
+    // TODO: Submit the checkout form
+
+    // Finally, close the browser
+    await browser.close();
   } catch (err) {
-    // If any error is throw, log it to the console
+    // On any error, log the error to the console, close the browser, and exit with an error code
     log(err);
-  } finally {
-    // Finally, close the browser instance
-    browser.close;
+    browser.close();
+    exit(1);
   }
+
+  // If everything succeeds, log out the results of the script and exit with a success code
   log('Fin!');
   exit(0);
 })();
